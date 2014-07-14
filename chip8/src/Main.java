ea6368755b67d008af2f68cd8dc73062f8cbab4a
vicboma <el_bolinches@hotmail.com>
2014-07-14 21:14:51 +0200

    Create Main

---
+++ chip8/src/Main.java
@@ -0,0 +1,26 @@
+import emulator.utils.Utils;
+
+import javax.swing.*;
+
+/**
+ * Created by vicboma on 08/07/14.
+ */
+public class Main {
+
+    private static final String SYSTEM_LOOK_AND_FEEL_CLASS_NAME = UIManager.getSystemLookAndFeelClassName();
+    private static final String EMULATOR_LOADED = "Emulator loaded ";
+    private static final String EMULATOR_LOADED_KO = EMULATOR_LOADED + "KO!";
+    private static final String EMULATOR_LOADED_OK = EMULATOR_LOADED + "OK!";
+
+    public static void main(String[] args) {
+        final Chip8 chip8 = new Chip8();
+        try {
+            Utils.setLookAndFeel(SYSTEM_LOOK_AND_FEEL_CLASS_NAME, chip8.windows());
+        } catch (Exception e) {
+            System.out.println(EMULATOR_LOADED_KO);
+            e.printStackTrace();
+        } finally {
+            System.out.println(EMULATOR_LOADED_OK);
+        }
+    }
+}
