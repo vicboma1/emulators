d376ed0312f729ca42b99876fa587bd51f100b23
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-08-03 22:10:43 +0200

    Rejected commit: Default

---
+++ gameboyclassic/src/emulator/utils/Logger.java
@@ -0,0 +1,10 @@
+package emulator.utils;
+
+/**
+ * Created by vicboma on 02/01/15.
+ */
+public class Logger {
+    static public void Debug(String s) {
+        System.out.println("Debug: " + s);
+    }
+}
