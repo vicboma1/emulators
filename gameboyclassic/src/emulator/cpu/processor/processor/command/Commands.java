cb4350959c7314a04ada6bfd3d366e418b1ba441
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-08-15 11:56:20 +0200

    delete duplicated

--- gameboyclassic/src/emulator/cpu/processor/processor/command/Commands.java
+++
@@ -1,8 +0,0 @@
-package emulator.cpu.processor.processor.command;
-
-/**
- * Created by vicboma on 12/08/15.
- */
-public interface Commands {
-    void runAsync(int opCode1, int opCode2, int opCode3, int opCode4);
-}
