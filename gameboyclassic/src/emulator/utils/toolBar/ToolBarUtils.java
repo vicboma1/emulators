7a3c686dfc76ef56d920e7581882e942e4b275fc
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-10-05 17:42:30 +0200

    Add toolBar Utils src & api

--- gameboyclassic/src/emulator/utils/toolBar/ToolBarUtils.java
+++ gameboyclassic/src/emulator/utils/toolBar/ToolBarUtils.java
@@ -1,10 +1,21 @@
 package emulator.utils.toolBar;
 
+import framework.injector.api.type.Injector;
+import framework.pattern.action.ActionInjector;
+import toolBar.others.action.ToolBarOthersItemActionImpl;
+
 import java.awt.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vicboma on 30/09/15.
  */
 public interface ToolBarUtils {
-    Menu createMenu(String menuName, java.util.List<MenuItem> menuItemList);
-    }
+    <T extends MenuItem> Menu createMenu(String menuName, List<T> menuItemList);
+    Menu addSubMenu(Menu menu, List<MenuItem> menuItemList);
+    void logError(String command, String message);
+    Boolean commander(String command, Map<String, ActionInjector> map, String message);
+    void addOptionAsync(Injector injector, String openRom);
+    ToolBarOthersItemActionImpl getToolBarOthersItemActionImpl(Injector injector);
+}
--- gameboyclassic/src/emulator/utils/toolBar/ToolBarUtilsImpl.java
+++ gameboyclassic/src/emulator/utils/toolBar/ToolBarUtilsImpl.java
@@ -4,9 +4,14 @@ import framework.injector.api.annotation.Inject;
 import framework.injector.api.annotation.method.PostConstruct;
 import framework.injector.api.type.Injector;
 import framework.logger.api.Logger;
+import framework.pattern.action.ActionInjector;
+import toolBar.others.action.ToolBarOthersItemAction;
+import toolBar.others.action.ToolBarOthersItemActionImpl;
+import utils.mainLoop.MainLoopUtils;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vicboma on 30/09/15.
@@ -26,9 +31,17 @@ public class ToolBarUtilsImpl implements ToolBarUtils {
         logger.info(this);
     }
 
+    /**
+     * Optimizado para cualquier tipo de menu item (check / radio / normal ...)
+     * @param menuName
+     * @param menuItemList
+     * @param <T>
+     * @return
+     */
     @Override
-    public Menu createMenu(String menuName, List<MenuItem> menuItemList){
+    public <T extends MenuItem> Menu createMenu(String menuName, List<T> menuItemList){
         final Menu menu = injector.getInstance(Menu.class);
+
         menu.setLabel(menuName);
         menuItemList.stream().forEach(x -> {
             if (x == null)
@@ -40,4 +53,46 @@ public class ToolBarUtilsImpl implements ToolBarUtils {
         return menu;
     }
 
+    @Override
+    public Menu addSubMenu(Menu menu, List<MenuItem> menuItemList){
+        menuItemList.stream().forEach(x -> {
+            if (x == null)
+                menu.addSeparator();
+            else
+                menu.add(x);
+        });
+
+        return menu;
+    }
+
+
+    public void logError(String command, String message) {
+        logger.error(this, message + command);
+    }
+
+    public Boolean commander(String command, Map<String,ActionInjector> map, String message) {
+        final ActionInjector toolBarFileAction = map.get(command);
+        if (toolBarFileAction != null) {
+            try {
+                logger.info(this, message +command);
+                    toolBarFileAction.execute(injector);
+            } catch (Exception ex) {
+                logger.error(this, ex.getMessage(), ex);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    //
+    public void addOptionAsync(Injector injector, String openRom) {
+        synchronized (injector) {
+            final MainLoopUtils mainLoopUtils = injector.getInstance(MainLoopUtils.class);
+            mainLoopUtils.addOptionAsync(injector, openRom);
+        }
+    }
+
+    public ToolBarOthersItemActionImpl getToolBarOthersItemActionImpl(Injector injector) {
+        return (ToolBarOthersItemActionImpl) injector.getInstance(ToolBarOthersItemAction.class);
+    }
 }
