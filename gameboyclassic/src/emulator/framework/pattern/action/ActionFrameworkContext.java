fad2895c27a31b1af3ca9ee1f50bd98fae2039b1
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-10-05 17:47:16 +0200

    add actions api

---
+++ gameboyclassic/src/emulator/framework/pattern/action/ActionFrameworkContext.java
@@ -0,0 +1,10 @@
+package emulator.framework.pattern.action;
+
+import framework.context.api.FrameworkContext;
+
+/**
+ * Created by vicboma on 05/10/15.
+ */
+public interface ActionFrameworkContext {
+   void execute(FrameworkContext frameworkContext);
+}
---
+++ gameboyclassic/src/emulator/framework/pattern/action/ActionInjector.java
@@ -0,0 +1,11 @@
+package emulator.framework.pattern.action;
+
+import framework.injector.api.type.Injector;
+
+/**
+ * Created by vicboma on 05/10/15.
+ */
+public interface ActionInjector {
+    void execute(Injector injector);
+
+}
